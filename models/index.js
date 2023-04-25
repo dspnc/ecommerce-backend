9c61999e9df3327484091bfc2118d0a8cff387e0
dspnc <dspence.hill@gmail.com>
2023-04-25 15:31:43 -0400

    category-routes updated and product category changed to allow null

--- models/index.js
+++ models/index.js
@@ -5,11 +5,11 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, { foreignKey: 'category_id' });
+Product.belongsTo(Category, { foreignKey: 'category_id', onDelete: 'CASCADE' });
 
 
 // Categories have many Products
-Category.hasMany(Product, { foreignKey: 'category_id'});
+Category.hasMany(Product, { foreignKey: 'category_id', onDelete: 'CASCADE'});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
